bedc97ae083ba584fec15a1bd4b5ad6a15dd7903
lylova11 <nemova94@mail.ru>
2022-06-09 21:11:32 +0300

    Initial commit

---
+++ mini-market/src/test/java/ru/geekbrains/mini/market/CreateProductTest.java
@@ -0,0 +1,58 @@
+package ru.geekbrains.mini.market;
+
+import com.github.javafaker.Faker;
+import ru.geekbrains.mini.market.dto.Product;
+import ru.geekbrains.mini.market.api.ProductService;
+import ru.geekbrains.mini.market.utils.RetrofitUtils;
+import lombok.SneakyThrows;
+import okhttp3.ResponseBody;
+import org.hamcrest.CoreMatchers;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import retrofit2.Response;
+import java.io.IOException;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+
+
+public class CreateProductTest {
+
+	static ProductService productService;
+	Product product = null;
+	Faker faker = new Faker();
+	int id;
+
+	@BeforeAll
+	static void beforeAll() {
+		productService = RetrofitUtils.getRetrofit()
+				.create(ProductService.class);
+	}
+
+	@BeforeEach
+	void setUp() {
+		product = new Product()
+				.withTitle(faker.food().ingredient())
+				.withCategoryTitle("Food")
+				.withPrice((int) (Math.random() * 10000));
+	}
+
+	@Test
+	void createProductInFoodCategoryTest() throws IOException {
+		Response<Product> response = productService.createProduct(product)
+				.execute();
+		id =  response.body().getId();
+		assertThat(response.isSuccessful(), CoreMatchers.is(true));
+	}
+
+	@SneakyThrows
+	@AfterEach
+	void tearDown() {
+		Response<ResponseBody> response = productService.deleteProduct(id).execute();
+		assertThat(response.isSuccessful(), CoreMatchers.is(true));
+	}
+
+
+
+}
